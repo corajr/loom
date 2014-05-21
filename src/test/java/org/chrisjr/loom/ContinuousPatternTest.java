ad0e028676fd0550c8525b8fb2a59e95791cee24
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2014-05-21 14:02:05 -0400

    adding tests for sine

--- src/test/java/org/chrisjr/loom/ContinuousPatternTest.java
+++ src/test/java/org/chrisjr/loom/ContinuousPatternTest.java
@@ -1,7 +1,8 @@
 package org.chrisjr.loom;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import org.chrisjr.loom.continuous.*;
 import org.chrisjr.loom.time.*;
@@ -37,9 +38,9 @@ public class ContinuousPatternTest {
 		scheduler.setElapsedMillis(500);
 		assertThat(pattern.getValue(), is(closeTo(0.0, epsilon)));
 		scheduler.setElapsedMillis(750);
-		assertThat(pattern.getValue(), is(closeTo(1.0, epsilon)));
+		assertThat(pattern.getValue(), is(closeTo(-1.0, epsilon)));
 		scheduler.setElapsedMillis(1000);
-		assertThat(pattern.getValue(), is(equalTo(0.0)));
+		assertThat(pattern.getValue(), is(closeTo(0.0, epsilon)));
 	}
 
 }
