f430911e7b2a4e9b5128d528e0afa759e1183b3e
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2014-08-12 12:45:12 -0400

    add assertion to audiosample test

--- src/test/java/org/chrisjr/loom/AsAudioSampleTest.java
+++ src/test/java/org/chrisjr/loom/AsAudioSampleTest.java
@@ -1,25 +1,66 @@
 package org.chrisjr.loom;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.chrisjr.loom.time.NonRealTimeScheduler;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import ddf.minim.*;
+import ddf.minim.javasound.JSMinim;
+import ddf.minim.spi.AudioOut;
 
 import java.io.*;
 import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AsAudioSampleTest {
+	private JSMinim jsMinim;
 	private Minim minim;
 	private Loom loom;
 	private NonRealTimeScheduler scheduler;
 	private Pattern pattern;
+
+	private final AtomicInteger samplesTriggered = new AtomicInteger();
+	private AudioOut fakeOut;
+
+	private class SampleWrapper extends AudioSample {
+		private final AudioSample original;
+
+		public SampleWrapper(AudioOut out, AudioSample original) {
+			super(out);
+			this.original = original;
+		}
+
+		@Override
+		public float[] getChannel(int arg0) {
+			return original.getChannel(arg0);
+		}
+
+		@Override
+		public AudioMetaData getMetaData() {
+			return original.getMetaData();
+		}
+
+		@Override
+		public int length() {
+			return original.length();
+		}
+
+		@Override
+		public void stop() {
+			original.stop();
+		}
+
+		@Override
+		public void trigger() {
+			samplesTriggered.incrementAndGet();
+			original.trigger();
+		}
+
+	}
 
 	// require methods for creating Minim on this
 	public String sketchPath(String fileName) {
@@ -48,11 +89,14 @@ public class AsAudioSampleTest {
 
 	@Before
 	public void setUp() throws Exception {
-		minim = new Minim(this);
+		jsMinim = new JSMinim(this);
+		minim = new Minim(jsMinim);
 
 		scheduler = new NonRealTimeScheduler();
 		loom = new Loom(null, scheduler);
 		pattern = new Pattern(loom);
+
+		fakeOut = jsMinim.getAudioOutput(2, 1024, 44100, 8);
 
 		loom.play();
 	}
@@ -64,11 +108,13 @@ public class AsAudioSampleTest {
 
 	@Test
 	public void audioSample() {
-		AudioSample snare = minim.loadSample("snare.aif");
+		AudioSample snareReal = minim.loadSample("snare.aif");
+		AudioSample snare = new SampleWrapper(fakeOut, snareReal);
 		pattern.extend("1101");
 
 		pattern.asSample(snare);
 
 		scheduler.setElapsedMillis(1001);
+		assertThat(samplesTriggered.get(), is(equalTo(3)));
 	}
 }
