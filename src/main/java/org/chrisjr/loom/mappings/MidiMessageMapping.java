3071e8ab125cf7c5a27880206fd7e68bb3e803e0
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2014-08-20 12:32:40 -0400

    ignore < 0 midi data

--- src/main/java/org/chrisjr/loom/mappings/MidiMessageMapping.java
+++ src/main/java/org/chrisjr/loom/mappings/MidiMessageMapping.java
@@ -23,7 +23,7 @@ public class MidiMessageMapping implements Mapping<MidiMessage> {
 		int channel = patterns[1].asMidiChannel();
 		int data1 = patterns[2].asMidiData1();
 
-		if (command == -1 || channel == -1 || data1 == -1)
+		if (command < 0 || channel < 0 || data1 < 0)
 			return null;
 
 		int data2 = patterns.length > 3 && patterns[3] != null ? patterns[3]
