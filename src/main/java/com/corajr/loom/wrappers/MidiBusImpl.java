1c3b362b26aa1f44b609a0b8a44bf6e0897ba251
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2016-10-09 17:31:01 -0400

    include getMidiBus function

--- src/main/java/com/corajr/loom/wrappers/MidiBusImpl.java
+++ src/main/java/com/corajr/loom/wrappers/MidiBusImpl.java
@@ -1,5 +1,7 @@
 package com.corajr.loom.wrappers;
 
+import processing.app.Platform;
+import processing.core.PApplet;
 import javax.sound.midi.MidiMessage;
 
 import themidibus.MidiBus;
@@ -21,4 +23,21 @@ public class MidiBusImpl implements IMidiBus {
 		midiBus.dispose();
 	}
 
+	public static MidiBus getDefaultMidiBus(PApplet app) {
+		MidiBus theBus = null;
+		if (Platform.isMacOS()) {
+			theBus = new MidiBus(app, "Bus 1", "Bus 1");
+		} else if (Platform.isWindows()) {
+			theBus = new MidiBus(app, -1, "Microsoft GS Wavetable Synth");			
+		} else {
+			System.err.println("Could not find MIDI device.");
+			System.out.println("Please create a MidiBus with the appropriate"
+					+ " outputs for your operating system.");
+			System.out.println("e.g. myBus = new MidiBus(this, -1, \"My Output\")");
+			System.out.println("Here are the currently available MIDI devices:");
+			MidiBus.list();
+			app.exit();
+		}
+		return theBus;
+	}
 }
